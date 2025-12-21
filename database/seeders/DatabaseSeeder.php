2df174104285b64b87d487c26f75cacdd2a84f43
mmmmohammadalhomsi@gmail.com <mmmmohammadalhomsi@gamil.com>
2025-12-21 08:30:59 +0300

    add two default users

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -27,7 +27,7 @@ class DatabaseSeeder extends Seeder
         User::create([
             'first_name' => 'Mohammad',
             'last_name' => 'Al Homsi',
-            'profileImage' => '/storage/app/public/mohammadHaboosh.jpg',
+            'profileImage' => '/storage/app/public/mohammadAlHomsi.jpg',
             'personIdImage' => 'https://www.pngall.com/wp-content/uploads/5/Admin-Profile-PNG-Image.png',
             'birthdate' => '2005-06-05',
             'verification_state' => 'verified',
@@ -41,7 +41,7 @@ class DatabaseSeeder extends Seeder
         User::create([
             'first_name' => 'Mohammad',
             'last_name' => 'Haboosh',
-            'profileImage' => '/storage/app/public/mohammadAlHomsi.jpg',
+            'profileImage' => '/storage/app/public/mohammadHaboosh.jpg',
             'personIdImage' => 'https://www.pngall.com/wp-content/uploads/5/Admin-Profile-PNG-Image.png',
             'birthdate' => '2004-07-08',
             'verification_state' => 'verified',
